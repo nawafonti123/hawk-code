d04895d3d5ebcb4d00a938653f7e4cbe3a47bcd0
nawafonti123 <132528240+nawafonti123@users.noreply.github.com>
2026-08-17 22:45:39 +0300

    feat: add translations for new agent capabilities

---
+++ apps/desktop/src/i18n-extensions.ts
@@ -0,0 +1,7 @@
+import i18n from "./i18n";
+
+i18n.addResource("en", "translation", "activity.browser_control", "Using the browser");
+i18n.addResource("ar", "translation", "activity.browser_control", "استخدام المتصفح");
+
+i18n.addResource("en", "translation", "attachment.preview.close", "Close preview");
+i18n.addResource("ar", "translation", "attachment.preview.close", "إغلاق المعاينة");
